dea6fc9a11058a6ece05008c792f1a294a4d06d0
veselovese <motte_vessale@mail.ru>
2024-09-26 19:15:35 +0300

    menu design

--- back-files/get-user-friends.php
+++ back-files/get-user-friends.php
@@ -2,5 +2,7 @@
 require_once('connect.php');
 $current_user_id = $_SESSION['user']['id'];
 
-$result_friend_1 = $connect->query("SELECT user_id_1 FROM friends JOIN users ON friends.user_id_1 = users.id WHERE user_id_2 = $current_user_id");
-$result_friend_2 = $connect->query("SELECT user_id_2 FROM friends JOIN users ON friends.user_id_2 = users.id WHERE user_id_1 = $current_user_id");
+$result_friend_1 = $connect->query("SELECT * FROM friends JOIN users ON friends.user_id_1 = users.id WHERE user_id_2 = $current_user_id");
+$result_friend_2 = $connect->query("SELECT * FROM friends JOIN users ON friends.user_id_2 = users.id WHERE user_id_1 = $current_user_id");
+
+$result_request_to = $connect->query("SELECT * FROM requests WHERE user_id_to = $current_user_id");
