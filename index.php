300fdce303ff517d165a16f3fd0942a22333450e
veselovese <motte_vessale@mail.ru>
2025-03-29 23:22:33 +0300

    new favicon

--- index.php
+++ index.php
@@ -5,6 +5,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1">
     <title>Rampus (Рампус)</title>
+    <link rel="apple-touch-icon" sizes="180x180" href="favicons/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="favicons/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="favicons/favicon-16x16.png">
+    <link rel="manifest" href="favicons/site.webmanifest">
 </head>
 
 <body>
