681541c4a23e4d95763b48d67fd3cef30eafef73
veselovese <motte_vessale@mail.ru>
2025-04-01 12:06:48 +0300

    2.5.0

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <footer>
     <div>
         <p>Rampus (Рампус), 2024-2025</p>
-        <p>Версия 2.5.0 beta</p>
+        <p>Версия 2.5.0</p>
     </div>
     <a href="https://t.me/rampusru" target="_blank"><img src="pics/TelegramIcon.svg" alt="Официальный telegram-канал"></a>
 </footer>
