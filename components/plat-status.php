1db01aea7bd0c07bd760c2c5878ad48d511a7914
veselovese <motte_vessale@mail.ru>
2026-01-07 17:03:16 +0300

    some fixs. 3.0.0 beta

--- components/plat-status.php
+++ components/plat-status.php
@@ -1,5 +1,7 @@
 <?php
 if ($other_user_plat_status) {
+    echo "<div class='plat-status-div'>";
     echo "<span id='plat-status'>PLɅT</span>";
-    echo "<span id='plat-status-desc'>Тестируем платные функции Rampus Platinum</span>";
+    echo "<span id='plat-status-desc'>Тестируем платные функции<br class='mobile-br'> Rampus Platinum</span>";
+    echo "</div>";
 }
